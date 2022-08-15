439a954c925f6625147ae9a22d71b801ec754620
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-08-14 17:47:52 -0700

    Add files via upload

---
+++ week1/math/app.js
@@ -0,0 +1,9 @@
+const mathApp = require('./math')
+
+console.log(mathApp.addition(4, 5))
+
+console.log(mathApp.subtraction(9, 2))
+
+console.log(mathApp.multiply(25, -4))
+
+console.log(mathApp.divide(75, 6))
---
+++ week1/math/math.js
@@ -0,0 +1,22 @@
+const addition = (a,b) =>{
+    return a + b;
+}
+
+const subtraction = (a,b) =>{
+    return a - b;
+}
+
+const multiply = (a,b) =>{
+    return a * b;
+}
+
+const divide = (a,b) =>{
+    return a / b;
+}
+
+module.exports = {
+    addition,
+    subtraction,
+    multiply,
+    divide
+}
